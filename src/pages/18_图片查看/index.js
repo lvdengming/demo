d6eed7bd7095bfe02f00e65e7f2fe4a2b949ca1a
lvdengming <lvdengming@foxmail.com>
2025-01-06 23:25:55 +0800

    feat: 图片查看案例-添加加载完成处理

--- src/pages/18_图片查看/index.js
+++ src/pages/18_图片查看/index.js
@@ -7,6 +7,31 @@ const app = document.getElementById('app');
 const img = document.querySelector('img');
 
 img.addEventListener('load', () => {
-    console.log(app, img);
-    console.log('图片加载完成');
+    // 图片加载完成后，保持图片宽高比在 app 中 object-fit contain 展示
+    const appRatio = app.clientWidth / app.clientHeight;
+    const imgRatio = img.naturalWidth / img.naturalHeight;
+
+    let width, height;
+    if (appRatio <= imgRatio) {
+        width = app.clientWidth;
+        height = width / imgRatio;
+    } else {
+        height = app.clientHeight;
+        width = height * imgRatio;
+    }
+
+    const top = (app.clientHeight - height) / 2;
+    const left = (app.clientWidth - width) / 2;
+
+    img.style.cssText = `
+        top: ${top}px;
+        left: ${left}px;
+        width: ${width}px;
+        height: ${height}px;
+        object-fit: unset;
+    `;
+
+    addEvents();
 });
+
+function addEvents() {}
