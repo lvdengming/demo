dc90ba1c746731c473bf0a15d1c075144e512008
lvdengming <lvdengming@foxmail.com>
2025-01-07 07:49:25 +0800

    feat: 图片查看案例-完成事件交互部分, 基本完成

--- src/pages/18_图片查看/index.js
+++ src/pages/18_图片查看/index.js
@@ -7,6 +7,8 @@ const app = document.getElementById('app');
 const img = document.querySelector('img');
 
 img.addEventListener('load', () => {
+    if (app.clientHeight === 0 || img.naturalHeight === 0) return;
+
     // 图片加载完成后，保持图片宽高比在 app 中 object-fit contain 展示
     const appRatio = app.clientWidth / app.clientHeight;
     const imgRatio = img.naturalWidth / img.naturalHeight;
@@ -34,4 +36,61 @@ img.addEventListener('load', () => {
     addEvents();
 });
 
-function addEvents() {}
+function addEvents() {
+    let startX,
+        startY,
+        startLeft,
+        startTop,
+        isDragging = false,
+        scale = 1;
+
+    img.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.clientX;
+        startY = e.clientY;
+        startLeft = img.offsetLeft;
+        startTop = img.offsetTop;
+    });
+
+    img.addEventListener('dragstart', (e) => {
+        e.preventDefault();
+    });
+
+    img.addEventListener('wheel', (e) => {
+        e.preventDefault();
+
+        scale += e.deltaY * -0.01;
+        // 限定缩放比例在 0.25-4 之间
+        scale = Math.min(Math.max(0.25, scale), 4);
+
+        img.style.transform = `scale(${scale})`;
+    });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+
+        // 限定拖拽范围在容器内
+        const rect = app.getBoundingClientRect();
+        if (
+            !(
+                e.clientX >= rect.left &&
+                e.clientX <= rect.right &&
+                e.clientY >= rect.top &&
+                e.clientY <= rect.bottom
+            )
+        ) {
+            isDragging = false;
+            return;
+        }
+
+        const diffX = e.clientX - startX;
+        const diffY = e.clientY - startY;
+
+        img.style.left = `${startLeft + diffX}px`;
+        img.style.top = `${startTop + diffY}px`;
+    });
+}
